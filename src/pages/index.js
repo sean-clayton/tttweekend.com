7d1710d0c81b23071ff1dc02e3176b6dcae82ec2
Sean Clayton <sean@seanclayton.me>
2020-08-18 02:18:18 -0400

    Better conditional rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -240,12 +240,11 @@ export default function Home() {
           tw="py-24 bg-blue-500 shadow-xl border-t border-solid border-blue-400"
           style={{ backgroundImage: pattern1Url }}
         >
-          <div style={{ display: weekend ? "block" : "none" }}>
+          {weekend ? (
             <Playing gmod={metadata.gmod} />
-          </div>
-          <div style={{ display: weekend ? "none" : "block" }}>
+          ) : (
             <NotPlaying discord={metadata.discord} />
-          </div>
+          )}
         </section>
 
         <section tw="bg-yellow-50 shadow-xl">
